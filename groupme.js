d2e2362a48aff6c6abc58a3661101130e38bc833
Alex Harelick <harelick@seas.upenn.edu>
2014-09-13 01:18:12 -0400

    almost got the groups happening

--- groupme.js
+++ groupme.js
@@ -2,9 +2,10 @@ var request = require('request');
 var express = require('express');
 var router = express.Router();
 
-var base = "http://localhost:3000/"
+var base = 'http://localhost:3000/'
 var clientId = 'SsOMWhuXnXPHdrOjD67FMyfOcBm2XumCBUUrC0n5btiSbTVt';
 var authorizeUrl = 'https://oauth.groupme.com/oauth/authorize?client_id=' + clientId;
+var apiEndpoint = 'https://api.groupme.com/v3'
 
 router.get('/', function(req, res) {
 	return res.redirect(authorizeUrl);
@@ -12,14 +13,42 @@ router.get('/', function(req, res) {
 
 
 router.get('/authorize', function(req, res) {
-  return res.redirect(authorizeUrl);
+  if (req.session.groupme_token) {
+    return res.redirect(base + 'main');
+  } else {
+    return res.redirect(authorizeUrl);
+  }
 });
 
 router.get('/success', function(req, res) {
-  req.session.token = req.query.access_token;
-  res.redirect(base + 'main');
+  req.session.groupme_token = req.query.access_token;
+  return res.redirect(base + 'main');
+});
 
+router.get('/groups', function(req, res) {
+  console.log(req.session.groupme_token);
+  request.get({
+    url: apiEndpoint + '/groups' + '?token=' + req.session.groupme_token
+  }, function(err, response, body) {
+    if (err) {
+      return res.redirect('http://en.wikipedia.org/wiki/HTTP_404');
+    }
 
+    var data = (JSON.parse(body)).response;
+    for (var i in data) {
+      console.log(data[i].name);
+    }  
+    /*req.db.collection('payments').insert({
+      amount: amount,
+      phone: phone,
+      note: note,
+      recipient: recipient,
+      date: Date.now()
+    }, function(err, result) {
+      req.session.message = 'Sent $' + amount + ' to ' + recipient + ' successfully!';
+    }); */
+      return res.redirect(base + 'main');
+  });
 });
 
 module.exports = router;
