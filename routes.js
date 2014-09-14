9dd9f89a81ce6f3b3e3816e8776e909a2e3bd5e3
Corey Loman <loman@seas.upenn.edu>
2014-09-14 16:41:51 -0400

    small changes

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ var ObjectID = require('mongodb').ObjectID;
 router.get('/', function(req, res) {
   req.db.collection('notes').find().toArray(function(err, notes) {
     return res.render('index', {
-      title: 'Codename: KND',
+      title: 'GroupMe+',
       notes: notes
     });
   });
@@ -14,40 +14,6 @@ router.get('/', function(req, res) {
 
 router.get('/main/', function(req, res) {
   return res.render('main');
-});
-
-router.get('/db', function(req, res) {
-  req.db.collection('test').find().toArray(function(err, items) {
-    return res.send(items);
-  });
-});
-
-router.get('/:id', function(req, res) {
-  req.db.collection('notes').findOne({_id: ObjectID(req.params.id)}, function(err, note) {
-    if (err || !note) {
-      req.session.message = 'That note does not exist!';
-      return res.redirect('/');
-    }
-
-    return res.render('note', {
-      note: note
-    })
-  });
-});
-
-router.post('/create', function(req, res) {
-  if (!(req.body.title && req.body.body)) {
-    req.session.message = 'You must provide a title and a body!';
-    return res.redirect('/');
-  }
-
-  req.db.collection('notes').insert({
-    title: req.body.title,
-    body: req.body.body
-  }, function(err, result) {
-    req.session.message = 'Note created!';
-    return res.redirect('/');
-  });
 });
 
 
